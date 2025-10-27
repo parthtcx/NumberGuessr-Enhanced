08ec52eeb57263b17071d97f76a258d1522794cb
parthtcx <ww5pkq2snb@privaterelay.appleid.com>
2025-10-27 15:51:14 -0400

    Difficulty feature added.

--- numberguessing.py
+++ numberguessing.py
@@ -1,25 +1,46 @@
 import random
 
-print(f"------NUMBER GUESSR-----\nThe objective of the game is to guess the correct number within SEVEN attempts. Good luck!")
+print(f"------NUMBER GUESSR-----\nThe objective of the game is to guess the correct number within a range. Good luck!")
 
-upper_bound = int(input("Please enter the upper bound: "))
+# Difficulty Config
+difficulty = int(input("Please choose a difficulty level!\n1. Easy -- 10 Tries\n2. Medium -- 7 Tries\n3. Hard -- 5 Tries\n4. Expert -- 3 Tries\nSelect (1-4): "))
+
+if difficulty == 1:
+    difficulty_name = "Easy"
+    tries_left = 10
+elif difficulty == 2:
+    difficulty_name = "Medium"
+    tries_left = 7
+elif difficulty == 3:
+    difficulty_name = "Hard"
+    tries_left = 5
+elif difficulty == 4:
+    difficulty_name = "Expert"
+    tries_left = 3
+else:
+    tries_left = 7
+    difficulty_name = "Medium"
+    print(f"Invalid input! Defaulting to Medium difficulty!")
+
+print(f"You have selected {difficulty_name} and have {tries_left} attempts to guess the number.")
+
 lower_bound = int(input("Please enter the lower bound: "))
+upper_bound = int(input("Please enter the upper bound: "))
 
-number = random.randint(upper_bound, lower_bound)
-tries_left = 7
+number = random.randint(lower_bound, upper_bound)
 guess_counter = 0
 
-while tries_left >= guess_counter:
+while tries_left > guess_counter:
+    remaining_attempts = tries_left - guess_counter
+    guess = int(input(f"Enter your guess ({remaining_attempts} attempts left): "))
     guess_counter += 1
-    guess = int(input("Enter your guess: "))
-
-    if guess == number:
-        print(f"Congratulations! The number was {number} and you guessed it in {guess_counter} attempts.")
-        break
     
+    if guess == number:
+        print(f"Congratulations! The number was {number} and you guessed it in {guess_counter} tries.")
+        break
+    elif guess_counter >= tries_left:
+        print(f"Sorry, the number was {number}. Try again next time!")
     elif guess > number:
         print(f"Too high! Go lower!")
     elif guess < number:
         print(f"Too low! Go higher!")
-    elif guess_counter >= tries_left:
-        print(f"Sorry, the number was {number}. Try again next time!")
